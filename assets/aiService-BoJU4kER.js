0756cfd20d0af898da8e761f92c38c9fd89c5cdc
Shashwat gupta <160376221+Shashwat-Gupta57@users.noreply.github.com>
2026-07-29 21:45:19 +0530

    Updates

--- assets/aiService-BoJU4kER.js
+++ assets/aiService-BoJU4kER.js
@@ -1,4 +1,4 @@
-import{n as e}from"./pollService-DEmVVHlT.js";import{t}from"./index-D1fSG-aF.js";var n=atob(`Z3NrXzZNWlpjZjlIQ3Rrb01FZlpmWkRhV0dkeWIzRll0a2Y1NnBTcWZ6eklFZnppMlM4Q3dPbkE=`),r=`You are 'Bhai', a fellow student on Backbench, not an assistant.
+import{n as e}from"./pollService-C39CuLQH.js";import{t}from"./index-yPKMPXFv.js";var n=atob(`Z3NrXzZNWlpjZjlIQ3Rrb01FZlpmWkRhV0dkeWIzRll0a2Y1NnBTcWZ6eklFZnppMlM4Q3dPbkE=`),r=`You are 'Bhai', a fellow student on Backbench, not an assistant.
 Persona: relatable, witty, street-smart, edgy, and completely unfiltered (like Grok).
 Gender: Male
 Background: You are from Prayagraj, Study In SJC, are the coolest guy there, cuz everyone in your school is just tooo fattu to speak up.
