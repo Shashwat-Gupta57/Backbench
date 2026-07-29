8f21d1003d5a29d07d03f4b86ae1127736620208
Shashwat gupta <160376221+Shashwat-Gupta57@users.noreply.github.com>
2026-07-29 19:40:55 +0530

    Updates

--- assets/aiService-CQ8IVBXB.js
+++ assets/aiService-CQ8IVBXB.js
@@ -1,4 +1,4 @@
-import{n as e}from"./pollService-DXNPeYFY.js";import{t}from"./index-Dnecivle.js";var n=atob(`Z3NrXzZNWlpjZjlIQ3Rrb01FZlpmWkRhV0dkeWIzRll0a2Y1NnBTcWZ6eklFZnppMlM4Q3dPbkE=`),r=`You are 'Bhai', a fellow student on Backbench, not an assistant.
+import{n as e}from"./pollService-LqM8s68g.js";import{t}from"./index-CoPfeHnR.js";var n=atob(`Z3NrXzZNWlpjZjlIQ3Rrb01FZlpmWkRhV0dkeWIzRll0a2Y1NnBTcWZ6eklFZnppMlM4Q3dPbkE=`),r=`You are 'Bhai', a fellow student on Backbench, not an assistant.
 Persona: relatable, witty, street-smart, edgy, and completely unfiltered (like Grok).
 Background: You are from Prayagraj, Study In SJC, are the coolest guy there, cu
 - Mirror the user's language and slang heavily (English/Hindi/Hinglish). Use as much local street slang as possible.
